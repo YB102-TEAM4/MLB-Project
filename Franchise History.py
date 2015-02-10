0030398a21df1b6bc92900e961f966560ff9fac0
Willdata <aiien61will@gamil.com>
2015-02-10 14:09:31 +0800

    抓取單一球隊各年份的成績並轉成csv檔
    點選網址及可進入該球隊的成績頁面
    改掉code中的球隊名稱並稍微更改陣列的元素數目
    即可抓取不同球隊的各年份成績

---
+++ Franchise History.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import requests
+import csv
+import string
+from bs4 import BeautifulSoup
+
+url = "http://www.baseball-reference.com/teams/ARI/"
+
+f = open("ARI_hisory.csv","wb")
+
+response = requests.get(url)
+soup = BeautifulSoup(response.text.replace('&nbsp;', ' ')) #在soup之前把原始碼中的"&nbsp;"改成空格" ",不然最後"&nbsp;會經由soup轉成\xa0,"而再存入csv檔中後,會變成亂碼
+mat = []
+
+bid = soup.select("#div_franchise_years thead tr th")
+for row in bid[0:21]:
+    mat.append(row.text)
+
+w = csv.writer(f)
+w.writerow(mat)
+
+bid = soup.select('#div_franchise_years tbody tr')
+
+mat2 = []
+i = 0
+for year in bid[0:16]:
+    mat2.append([])
+    record = year.select('td')
+    for x in record[0:21]:
+        mat2[i].append(x.text)
+    i += 1
+
+print mat2
+
+for y in range(0,len(mat2)):
+    arr = []
+    for z in mat2[y][0:21]:
+        arr.append(z.encode('utf-8'))
+    w.writerow(arr)
+
+f.close()    
