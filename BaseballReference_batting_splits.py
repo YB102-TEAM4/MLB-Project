b94e73dc8f992967ebe726b9053508364ad7e48e
Willdata <aiien61will@gamil.com>
2015-02-26 17:12:29 +0800

    batting splits抓取的code的更正

--- BaseballReference_batting_splits.py
+++ BaseballReference_batting_splits.py
@@ -6,17 +6,29 @@ from bs4 import BeautifulSoup
 teamNameList = ['ARI', 'ATL', 'BAL', 'BOS', 'CHC', 'CHW', 'CIN', 'CLE', 'COL', 'ANA', 'DET', 'FLA', 'HOU', 'KCR' \
            , 'LAD', 'MIL', 'MIN', 'NYM', 'NYY', 'OAK', 'PHI', 'PIT', 'SDP', 'SEA', 'SFG', 'STL', 'TBD', 'TEX' \
            , 'TOR', 'WSN']#隊名
-#teamNameList = ['ARI']
-yearList = range(2005,2015)#年分
-#yearList = range(2014,2015)#年分
+
+yearList = range(2005,2015)#年分從2005到2014
+
+#file id
 idList = ['#total','#plato','#hmvis','#half','#month','#outcb','#stsub','#age','#defp','#leado','#lineu','#tkswg','#count','#outs', \
           '#bases','#clutc','#lever','#innng','#times','#power','#gbfb','#hitlo','#traj','#oppon','#stad','#site','#plats']#要抓取的資料的id
-endFileNameList = ['season_totals', 'platoon_splits', 'home_or_away', 'first_or_second_half', 'months', 'game_outcome_for_team', \
+
+#要建立的檔案名稱
+endFileNameList = ['Season Totals', 'Platoon Splits', 'Home or Away', 'First or Second Half', 'Months',\
+                   'Game Outcome for Team', 'Starter or Substitute', 'Player Age', 'Defensive Positions',\
+                   'Leading Off Inning', 'Batting Order Positions', 'Swung or Took First Pitch of PA',\
+                   'Count_Balls-Strikes', 'Number of Outs in Inning', 'Bases Occupied', 'Clutch Stats',\
+                   'Leverage', 'By Inning', 'Times Facing Opponent in Game', 'vs. Power_Finesse Pitchers',\
+                   'vs. Ground Ball_Fly Ball Pitchers', 'Hit Location', 'Hit Trajectory', 'Opponent',\
+                   'Game Conditions', 'Ballparks', 'Ballpark Platoon Splits']#file
+
+battingSplitsFileNameList = ['season_totals', 'platoon_splits', 'home_or_away', 'first_or_second_half', 'months', 'game_outcome_for_team', \
                    'starter_or_substitute', 'player_age', 'defensive_positions', 'leading_off_inning', 'batting_order_positions', \
                    'swung_or_took_first_pitch_of_pa', 'count_balls_strikes', 'number_of_outs_in_inning', 'bases_occupied', \
                    'clutch_stats', 'leverage', 'by_inning', 'times_facing_opponent_in_game', 'vs_power_finesse_pitchers', \
                    'vs_ground_ball_fly_ball_pitchers', 'hit_location', 'hit_trajectory', 'opponent', 'game_conditions', \
-                   'ballparks', 'ballpark_platoon_splits']#file
+                   'ballparks', 'ballpark_platoon_splits']#batting splits folder
+
 partFileName = '%s_%d_batting_splits_%s.csv'#檔案名稱
 folder='%s\\batting_splits\\%s\\'#資料夾名稱 第一個%s是隊名teamNameList 第二個%s是endFileNameList
 url="http://www.baseball-reference.com/teams/split.cgi?t=b&team=%s&year=%d"#網址
@@ -32,7 +44,7 @@ for year in yearList: #先依隊伍名稱和年份做兩次迴圈
         soup=BeautifulSoup(res.text.encode('utf8'))
         for n in range(0,27):#寫入27種表格 因為idList和endFileNameList是一對一關係 在這層迴圈會同時用這兩個
             thList=soup.select(idList[n]+' thead tr th')#標題
-            fod=open(folder%(teamName,endFileNameList[n])+partFileName%(teamName,year,endFileNameList[n]),'w')
+            fod=open(folder%(teamName,battingSplitsFileNameList[n])+partFileName%(teamName,year,endFileNameList[n]),'w')
             #用字串格式化 輸入完整資料夾路徑檔名
             
             for th in thList:                
@@ -48,4 +60,4 @@ for year in yearList: #先依隊伍名稱和年份做兩次迴圈
             fod.close()
         del res, soup
         print teamName + str(year) + ' over'
-#########################################################################################   
+#########################################################################################    
