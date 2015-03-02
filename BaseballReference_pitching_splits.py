d1e6fa1ec1c7577ea3d1230e37d94e6304e4a394
YB10216 <liangbug@gmail.com>
2015-03-02 22:47:47 +0800

    extra表格抓的到

--- BaseballReference_pitching_splits.py
+++ BaseballReference_pitching_splits.py
@@ -6,12 +6,13 @@ teamNameList = ['ARI', 'ATL', 'BAL', 'BOS', 'CHC', 'CHW', 'CIN', 'CLE', 'COL', '
            , 'LAD', 'MIL', 'MIN', 'NYM', 'NYY', 'OAK', 'PHI', 'PIT', 'SDP', 'SEA', 'SFG', 'STL', 'TBD', 'TEX' \
            , 'TOR', 'WSN']#隊名
 
+'''#無用,原因有extra的表格是用javascript生成 ,改用 theadList tbodyList
 idList = ['#total','#total_extra','#plato','#hmvis','#div_hmvis_extra','#half','#half_extra',\
           '#month','#month_extra','#outco','#outco_extra','#sprel','#sprel_extra','#age','#age_extra',\
           '#rs','#rs_extra','#lineu','#tkswg','#count','#leado','#defpo','#outs','#bases','#clutc',\
           '#lever','#innng','#times','#pitco','#dr','#dr_extra','#hitlo','#traj','#oppon','#oppon_extra',\
           '#stad','#stad_extra','#site','#site_extra','#plats','#catch','#ump','#ump_extra']#file id
-
+'''
 #要建立的檔案名稱
 endFileNameList = ['Season Totals', 'Season Totals_extra', 'Platoon Splits', 'Home or Away', 'Home or Away_extra',\
                    'First or Second Half', 'First or Second Half_extra', 'Months', 'Months_extra',\
@@ -36,15 +37,18 @@ pitchingSplitsFileNameList = ['season_totals', 'season_totals_extra', 'platoon_s
                    'by_catcher', 'by_umpire', 'by_umpire_extra']#pitching splits folder
 
 
-#-----------補上extra的資料夾--------------------------------------------------
-#for teamName in teamNameList:
-#    for endFileName in endFileNameList:
-#        if not os.path.exists(teamName+'\\pitching_splits\\'+endFileName):
-#            os.mkdir(teamName+'\\pitching_splits\\'+endFileName)
+#-----------建立資料夾--------------------------------------------------
+for teamName in teamNameList:
+    if not os.path.exists(teamName):
+        os.mkdir(teamName)
+    if not os.path.exists(teamName+'\\pitching_splits'):
+        os.mkdir(teamName+'\\pitching_splits')
+    for pitchingSplitsFileName in pitchingSplitsFileNameList:
+        if not os.path.exists(teamName+'\\pitching_splits\\'+pitchingSplitsFileName):
+            os.mkdir(teamName+'\\pitching_splits\\'+pitchingSplitsFileName)
 #-----------------------------------------------------------------------------
 
 yearList = range(2005,2015) #年分從2005到2014
-
 
 partFileName = '%s_%d_pitching_splits_%s.csv'    #檔案名稱
 folder='%s\\pitching_splits\\%s\\'    #資料夾名稱 第一個%s是隊名teamNameList 第二個%s是endFileNameList
@@ -59,21 +63,22 @@ for year in yearList: #先依隊伍名稱和年份做兩次迴圈
         else:
             res=requests.get(url%(teamName,year))#用字串格式化 輸入完整網址
         soup=BeautifulSoup(res.text.encode('utf8'))
-        for n in range(0,43):#寫入43種表格 因為idList和endFileNameList是一對一關係 在這層迴圈會同時用這兩個
-            thList=soup.select(idList[n]+' thead tr th')#標題
+        theadList=soup.select('thead')
+        tbodyList=soup.select('tbody')
+        for n in range(0,43):#寫入43種表格 因為idList和endFileNameList是一對一關係 在這層迴圈會同時用這兩個            
             fod=open(folder%(teamName,pitchingSplitsFileNameList[n])+partFileName%(teamName,year,endFileNameList[n]),'w')
             #用字串格式化 輸入完整資料夾路徑檔名
-
+            thList=theadList[n].select('tr th')#標題
             for th in thList:
-                fod.write(th.text.encode('utf8')+',')
+                fod.write(th.text.replace(',','').encode('utf8')+',')#注意有的欄位有','
             fod.write('\n')
             del thList
-            trList=soup.select(idList[n]+' tbody tr')#內容
+            trList=tbodyList[n].select('tr')#內容
             for tr in trList:
                 for td in tr.select('td'):
-                    fod.write(td.text.encode('utf8')+',')
+                    fod.write(td.text.replace(',','').encode('utf8')+',')#注意有的欄位有','
                 fod.write('\n')
             del trList
             fod.close()
-        del res, soup
+        del res, soup, theadList, tbodyList
         print teamName + str(year) + ' over'
